1ee505c942c850e83800b0ac30621caeee1ef6c0
Levi Starrett <levi@roxsoftware.com>
2014-09-02 13:36:39 -0400

    clean up libraries, keep cheat on when reset

--- play/js/lib/history.js
+++ play/js/lib/history.js
@@ -1,15 +1,24 @@
-// keeps an array of a specific length. when a string is added, the next string is pushed out
-var History = function(size) {
-    this.size = size;
-    this.history = new Array(size);
-    for (var i = 0; i < size; i++) this.history[i] = "";
-}
 
-History.prototype.add = function(string) {
-    this.history.shift();
-    this.history.push(string);
-}
+const History = (function() {
 
-History.prototype.toString = function() {
-    return this.history.join(",");
-}
+    "use strict";
+
+    // keeps an array of a specific length. when a string is added, the next string is pushed out
+    var History = function(size) {
+        this.size = size;
+        this.history = new Array(size);
+        for (var i = 0; i < size; i++) this.history[i] = "";
+    }
+
+    History.prototype.add = function(string) {
+        this.history.shift();
+        this.history.push(string);
+    }
+
+    History.prototype.toString = function() {
+        return this.history.join(",");
+    }
+
+    return History;
+
+})();
